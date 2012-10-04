f3dfefe3e71cd0d001e9d6d88e8509084b6da7f3
Alex Klimenkov <alex.klimenkov89@gmail.com>
2012-10-04 16:13:52 +0300

    update gem

--- lib/autotest-email.rb
+++ lib/autotest-email.rb
@@ -3,20 +3,35 @@ require 'mail'
 
 module Autotest
 
-  class << self
-    attr_accessor: :address, :port, :user_name, :password, :enable_ssl
-
-    def configure
-      yield self
-    end
-  end
-
   module Email
 
-    def get
+    class << self
+
+      attr_accessor :address, :port, :user_name, :password, :enable_ssl
+    
+      def configure
+	yield self
+      end
+    end
+
+    def find_email_by_subject(option={})
       init()
-      mail = Mail.first
-      p mail.envelope.from 
+      body = nil
+      while body == nil or time < 15 do
+	time += 1
+
+	Mail.all.each do |mail|
+	  if mail.to == option[:to] and mail.subject == option[:subject]
+	    body = mail.body
+	  else
+	    body = nil
+	  end
+	end
+
+	sleep 30
+      end
+      
+      body
     end
 
     private 
@@ -24,22 +39,21 @@ module Autotest
     def init()
       Mail.defaults do
 	retriever_method :pop3, 
-	  :address    => AutotestEmail.address,
-          :port       => AutotestEmail.port,
-          :user_name  => AutotestEmail.user_name,
-          :password   => AutotestEmail.password,
-          :enable_ssl => AutotestEmail.enable_ssl
+	  :address    => Email.address,
+          :port       => Email.port,
+          :user_name  => Email.user_name,
+          :password   => Email.password,
+          :enable_ssl => Email.enable_ssl
       end
-
     end
-  end
 
-AutotestEmail.configure do |config|
+  end
+end
+
+Autotest::Email.configure do |config|
   config.address = 'pop.gmail.com'
   config.port = 995
   config.user_name = 'example@gmail.com'
   config.password = 'password'
   config.enable_ssl = true
-end
-
 end
